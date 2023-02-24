cc3affe90e10209242402abcb22adf2be676a279
Franco Lepe <Francolepe169@gmail.com>
2023-02-24 06:56:59 -0500

    commit tree 7a52c967ac437a68320c98543bbcd38c484a2e9c

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+const employee ={
+    name: "sam",
+    streetAddress: "1234 main",
+}
+
+function updateEmployeeWithKeyAndValue(employee,name,value){
+    return {...employee,[key]:value}
+    
+}
